5aac7b9de95771cebd90c2107c75ecc169141a11
yana-mkr <yana.mkrtchyan@gmail.com>
2021-06-29 13:51:09 -0500

    not pulling edit post

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -2,7 +2,7 @@ async function editFormHandler(event) {
     event.preventDefault();
 
     const title = document.querySelector('#post-title').value.trim();
-    const post_text = document.querySelector('#post-body').value.trim();
+    const post_text = document.querySelector('#post-body').value;
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
@@ -14,7 +14,7 @@ async function editFormHandler(event) {
             post_text
         }),
         headers: {
-            'Content-Type:': 'application/json'
+            'Content-Type': 'application/json'
         }
     });
 
