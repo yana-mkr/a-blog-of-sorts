d09074f40a43caacddb8d1796976e5e0a2efe817
yana-mkr <yana.mkrtchyan@gmail.com>
2021-06-29 18:21:21 -0500

    fixed dashboard username

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,21 +2,16 @@ const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-// get all posts for dashboard
-router.get('/', withAuth, (req, res) => {
-    console.log(req.session);
-
+// get all posts for homepage
+router.get('/', (req, res) => {
     Post.findAll({
-        where: {
-            user_id: req.session.user_id
-        },
         attributes: [
             'id',
             'post_text',
             'title',
             'created_at'
         ],
-        included: [
+        include: [
             {
                 model: Comment,
                 attributes: [
@@ -39,9 +34,10 @@ router.get('/', withAuth, (req, res) => {
     })
         .then(dbPostData => {
             const post = dbPostData.map(post => post.get({ plain: true }));
+
             res.render('dashboard', {
                 post,
-                loggedIn: true
+                loggedIn: req.session.loggedIn
             });
         })
         .catch(err => {
@@ -50,24 +46,65 @@ router.get('/', withAuth, (req, res) => {
         });
 });
 
-// get one post to edit
-router.get('/edit/:id', withAuth, (req, res) => {
-    Post.findByPk(req.params.id)
-        .then(dbPostData => {
-            if (dbPostData) {
-                const post = dbPostData.get({ plain: true });
-
-                res.render('edit-post', {
-                    post,
-                    loggedIn: true
-                });
-            } else {
-                res.status(404).end();
+// get a single post
+router.get('/posts/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_text',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    'id',
+                    'comment_text',
+                    'post_id',
+                    'user_id',
+                    'created_at'
+                ],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
             }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id!' });
+                return;
+            }
+
+            const post = dbPostData.get({ plain: true });
+
+            res.render('edit-post', {
+                post,
+                loggedIn: req.session.loggedIn
+            });
         })
         .catch(err => {
+            console.log(err);
             res.status(500).json(err);
         });
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
 });
 
 module.exports = router;
